f1cedc48342240d87b40e26922913edf63840447
kyaw-min-khant <kyawminkhant8237@gmail.com>
2023-11-30 00:31:20 +0630

    Fix CORS Policy

--- server/index.js
+++ server/index.js
@@ -7,12 +7,7 @@ const auth = require("./Route/auth");
 const blog = require("./Route/blog");
 
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:5175",
-    "http://localhost:5176",
-  ],
+  origin: ["http://localhost:5173"],
   Credentials: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 };
