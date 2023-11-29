7824347b3f70ea34fe55f981aba70dc28bd4f793
kyaw-min-khant <kyawminkhant8237@gmail.com>
2023-11-30 00:40:37 +0630

    Fix CORS

--- server/index.js
+++ server/index.js
@@ -1,19 +1,13 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-require("dotenv").config();
-const cors = require("cors");
+
 const auth = require("./Route/auth");
 const blog = require("./Route/blog");
 
-const corsOptions = {
-  origin: ["http://localhost:5173"],
-  credentials: true,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-};
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors(corsOptions));
+
 app.use("/blog", blog);
 app.use("/auth", auth);
 
